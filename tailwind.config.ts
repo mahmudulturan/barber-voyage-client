1c6788779f12db3fcf045e7d2c145e09f23c4ddc
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-06 00:34:23 +0600

    tailwind color pallate setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        textCol: "#FFFFFF",
+        bgCol: "#F1F1F1",
+        primaryCol: "#F1775C",
+        seconderyCol: "#173253",
       },
     },
   },
