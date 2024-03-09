68a403efab7e24e466dce225976b0af80290203e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-09 14:27:47 +0600

    validate login form with hook form

--- src/app/(auth)/login/components/LoginForm.tsx
+++ src/app/(auth)/login/components/LoginForm.tsx
@@ -2,24 +2,62 @@
 import React, { useState } from 'react';
 import Button from '@/components/shared/Button/Button';
 import InputWithLabel from '@/components/shared/Input/InputWithLabel';
-import { FaSpinner } from 'react-icons/fa';
+import { FaEye, FaEyeSlash, FaSpinner } from 'react-icons/fa';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type LoginInputs = {
+    email: string
+    password: string
+}
 
 const LoginForm = () => {
     const [loading, setLoading] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<LoginInputs>()
 
 
-    // handler for login a user
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const form = e.target as HTMLFormElement;
-        const email = form.email.value;
-        console.log(email);
+    // handle for login user
+    const onSubmit: SubmitHandler<LoginInputs> = (data) => {
+        const email = data.email;
+        const password = data.password;
+        const userInfo = { email, password };
+        console.log(userInfo);
     }
+
     return (
-        <form action="" onSubmit={handleSubmit} className='space-y-4'>
-            <InputWithLabel name='email' placeholder='Email' id='email' />
-            <InputWithLabel name='password' placeholder='Password' id='password' />
-            <Button variant={"primaryReverse"} className='w-full' onClick={() => setLoading(pre => !pre)}>
+        <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+
+            {/* email input field */}
+            <InputWithLabel
+                {...register("email", { required: true })}
+                autoComplete='username'
+                name='email' placeholder='Email' id='email' type='email' />
+            {errors.email && <span className='text-red-400'>Email is required!</span>}
+
+            {/* password input field */}
+            <div className='relative'>
+                <InputWithLabel
+                    {...register("password", { required: true })}
+                    autoComplete="current-password"
+                    name='password' placeholder='Password' id='password' type={showPassword ? "text" : "password"} />
+
+                {/* show password toggle */}
+                <span
+                    className='absolute top-3 text-2xl right-3 cursor-pointer px-0.5 py-0.5'
+                    onClick={() => setShowPassword(pre => !pre)}
+                >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </span>
+            </div>
+            {errors.password && <span className='text-red-400'>Password is required!</span>}
+
+            {/* Login Button */}
+            <Button
+                variant={"primaryReverse"}
+                className='w-full'
+                type='submit'
+                onClick={() => setLoading(pre => !pre)}
+            >
                 {
                     loading ?
                         <FaSpinner className='text-2xl py-0.5 animate-spin' />
--- src/components/shared/Input/InputWithLabel.tsx
+++ src/components/shared/Input/InputWithLabel.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 
 interface InputWithLabelProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    placeholder: string
+    placeholder: string;
+    ref?: React.Ref<HTMLInputElement>;
 }
 
-const InputWithLabel = ({ placeholder, id, name, type, ...props }: InputWithLabelProps) => {
+const InputWithLabel = React.forwardRef<HTMLInputElement, InputWithLabelProps>(({ placeholder, id, name, type, ...props }, ref) => {
     return (
         <div className="relative z-0 w-full group">
-            <input type={type} name={name} id={id} {...props} className="block py-3 px-2 w-full text-sm text-black bg-transparent appearance-none focus:outline-none focus:ring-0 focus:border-primaryCol peer border-2 border-primaryCol rounded-md"
+            <input type={type} name={name} id={id} ref={ref} {...props} className="block py-3 px-2 w-full text-base text-black bg-transparent appearance-none focus:outline-none focus:ring-0 focus:border-primaryCol peer border-2 border-primaryCol rounded-md"
                 placeholder=" " />
             <label htmlFor={id} className="peer-focus:font-medium absolute text-base px-1 text-black duration-300 transform -translate-y-6 scale-80 top-[11px] origin-[0] peer-focus:start-3 start-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-black/90 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-80 peer-focus:-translate-y-[24px] bg-bgCol">
                 {placeholder}
             </label>
         </div>
     );
-};
+});
 
 export default InputWithLabel;
