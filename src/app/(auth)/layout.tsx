aa357e4341b251f53cb03ef29c5afe861af07253
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-09 14:43:08 +0600

    add: social login ui on auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import LoginRegisterTab from './components/LoginRegisterTab';
+import SocialLogin from './components/SocialLogin';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='flex items-center justify-center min-h-screen'>
-            <div className='min-h-[50vh] max-w-xl w-full border-2 rounded-md px-3 md:px-6 py-8'>
+            <div className='max-w-xl w-full border-2 rounded-md px-3 md:px-6 py-8'>
                 <LoginRegisterTab />
                 {children}
+                <SocialLogin />
             </div>
         </div>
     );
--- src/components/shared/Input/InputWithLabel.tsx
+++ src/components/shared/Input/InputWithLabel.tsx
@@ -10,7 +10,7 @@ const InputWithLabel = React.forwardRef<HTMLInputElement, InputWithLabelProps>((
         <div className="relative z-0 w-full group">
             <input type={type} name={name} id={id} ref={ref} {...props} className="block py-3 px-2 w-full text-base text-black bg-transparent appearance-none focus:outline-none focus:ring-0 focus:border-primaryCol peer border-2 border-primaryCol rounded-md"
                 placeholder=" " />
-            <label htmlFor={id} className="peer-focus:font-medium absolute text-base px-1 text-black duration-300 transform -translate-y-6 scale-80 top-[11px] origin-[0] peer-focus:start-3 start-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-black/90 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-80 peer-focus:-translate-y-[24px] bg-bgCol">
+            <label htmlFor={id} className="peer-focus:font-medium absolute text-base px-1 text-black duration-300 transform -translate-y-6 scale-80 top-[11px] origin-[0] peer-focus:start-3 start-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-black/90 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-80 peer-focus:-translate-y-[24px] bg-bgCol cursor-text">
                 {placeholder}
             </label>
         </div>
