49c0ff91e2818d86ccf79dfb973c02ab4f8b2a31
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-11 01:01:54 +0600

    add: loading file and change favicon

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+"use client"
+import Loading from '@/components/shared/Loading/Loading';
+import React from 'react';
+
+const loading = () => {
+    return <Loading/>;
+};
+
+export default loading;
