56f53f91dd78df12b8d8041bcbd22ad1a7340b28
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-10 10:45:34 +0600

    create endpoints for login user

--- src/redux/api/usersApi/usersApi.ts
+++ src/redux/api/usersApi/usersApi.ts
@@ -1,5 +1,6 @@
 import { RegisterInputs } from "@/app/(auth)/register/components/RegisterForm";
 import baseApi from "../baseApi";
+import { LoginInputs } from "@/app/(auth)/login/components/LoginForm";
 
 const usersApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
@@ -9,8 +10,15 @@ const usersApi = baseApi.injectEndpoints({
                 method: "POST",
                 body: data
             })
+        }),
+        loginUser: builder.mutation<LoginInputs, object>({
+            query: (data) => ({
+                url: '/api/v1/auth/login',
+                method: "POST",
+                body: data
+            })
         })
     })
 })
 
-export const { useRegisterUserMutation } = usersApi;
+export const { useRegisterUserMutation, useLoginUserMutation } = usersApi;
