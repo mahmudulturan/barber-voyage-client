4c7aa875ce3320c2548c2d5a8defcbc6f509cd4d
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-11 00:47:04 +0600

    feat: handle navbar color and visiblity while scrolling

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="min-h-screen flex flex-col bg-seconderyCol">
+        <div className="min-h-screen flex flex-col">
             <Navbar />
             <main className="flex-1">{children}</main>
             <Footer />
--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -9,14 +9,15 @@ import Button from '@/components/shared/Button/Button';
 import { useLazyLogOutUserQuery, } from '@/redux/api/usersApi/usersApi';
 import toast from 'react-hot-toast';
 import { removeUser } from '@/redux/slices/usersSlice/usersSlice';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+    const [isScrolling, setIsScrolling] = useState(false);
+    const [prevPosition, setPrevPosition] = useState(0);
     const user = useSelector((state: RootState) => state.usersSlice);
     const [logoutUser, { isLoading: isLogoutLoading }] = useLazyLogOutUserQuery();
 
     const dispatch = useDispatch();
-
-    console.log(user);
 
     // handler for logout user
     const handleLogout = () => {
@@ -33,8 +34,25 @@ const Navbar = () => {
             }
         );
     }
+
+    const onScroll = () => {
+        const currentPosition = window.scrollY;
+        /* isScrolling state will be true when scroll up and current scroll position greater than 50.
+         or current current position beetween 50-250 it will also return true */
+        setIsScrolling(prevPosition > currentPosition && currentPosition >= 50 || prevPosition < currentPosition && (currentPosition >= 50 && currentPosition <= 250));
+        setPrevPosition(currentPosition);
+    };
+
+    // handle sideeffect for scroll
+    useEffect(() => {
+        window.addEventListener('scroll', onScroll);
+        return () => {
+            window.removeEventListener('scroll', onScroll);
+        };
+    }, [prevPosition]);
+
     return (
-        <nav className='bg-transparent fixed w-full mb-24'>
+        <nav className={`fixed w-full z-40 mb-24 duration-300 ${isScrolling ? "bg-seconderyCol" : "bg-transparent"} ${isScrolling || (prevPosition <= 250) ? "" : " -translate-y-full"}`}>
             <div className='wrapper flex items-center justify-between py-4'>
                 <div>
                     <Link href={'/'} className='w-32'>
