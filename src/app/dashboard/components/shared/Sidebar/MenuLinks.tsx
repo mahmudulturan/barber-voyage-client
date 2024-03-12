7ee26c8739d0dd67682f54efc7807e3c3c6b781d
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-12 23:31:12 +0600

    use custom navlink instead of using link tag on sidebar

--- src/app/dashboard/components/shared/Sidebar/MenuLinks.tsx
+++ src/app/dashboard/components/shared/Sidebar/MenuLinks.tsx
@@ -10,6 +10,7 @@ import { GrUserWorker } from "react-icons/gr";
 import { FaSackDollar } from "react-icons/fa6";
 import { CgProfile } from "react-icons/cg";
 import NavLink from '@/components/shared/NavLink/NavLink';
+import { IoIosSettings } from "react-icons/io";
 
 const MenuLinks = () => {
     return (
@@ -17,53 +18,38 @@ const MenuLinks = () => {
             <div className='my-6'>
                 <h5 className='text-textCol'>Pages</h5>
                 <div className='flex flex-col gap-2 justify-center h-full my-2'>
-
                     <NavLink active='activeSidebar' other='sidebar' href='/dashboard'>
                         <MdDashboard className='text-xl' />
                         Dashboard
                     </NavLink>
-
-                    <Link href={'/'}>
-                        <Button variant={"sidebar"}>
-                            <CiShop className='text-xl' />
-                            Appointments
-                        </Button>
-                    </Link>
-                    <Link href={'/'}>
-                        <Button variant={"sidebar"}>
-                            <FaUsersGear className='text-xl' />
-                            Subscribers
-                        </Button>
-                    </Link>
-                    <Link href={'/'}>
-                        <Button variant={"sidebar"}>
-                            <FaSackDollar className='text-xl' />
-                            Transactions
-                        </Button>
-                    </Link>
-                    <Link href={'/'}>
-                        <Button variant={"sidebar"}>
-                            <GrUserWorker className='text-xl' />
-                            Manage Barbers
-                        </Button>
-                    </Link>
-                    <Link href={'/'}>
-                        <Button variant={"sidebar"} className='w-full justify-start gap-2'>
-                            <FaUsersGear className='text-xl' />
-                            Manage Customers
-                        </Button>
-                    </Link>
-                </div>
-            </div>
-            <div className='my-6'>
-                <h5 className='text-textCol'>Users</h5>
-                <div className='flex flex-col gap-2 justify-center h-full my-2'>
-                    <Link href={'/'}>
-                        <Button variant={"sidebar"}>
-                            <CgProfile className='text-xl' />
-                            Profile Settings
-                        </Button>
-                    </Link>
+                    <NavLink active='activeSidebar' other='sidebar' href='/'>
+                        <CiShop className='text-xl' />
+                        Appointments
+                    </NavLink>
+                    <NavLink active='activeSidebar' other='sidebar' href='/'>
+                        <FaUsersGear className='text-xl' />
+                        Subscribers
+                    </NavLink>
+                    <NavLink active='activeSidebar' other='sidebar' href='/'>
+                        <FaSackDollar className='text-xl' />
+                        Transactions
+                    </NavLink>
+                    <NavLink active='activeSidebar' other='sidebar' href='/'>
+                        <GrUserWorker className='text-xl' />
+                        Manage Barbers
+                    </NavLink>
+                    <NavLink active='activeSidebar' other='sidebar' href='/'>
+                        <FaUsersGear className='text-xl' />
+                        Manage Customers
+                    </NavLink>
+                    <NavLink active='activeSidebar' other='sidebar' href='/'>
+                        <IoIosSettings className='text-xl' />
+                        Manage Shop
+                    </NavLink>
+                    <NavLink active='activeSidebar' other='sidebar' href='/'>
+                        <CgProfile className='text-xl' />
+                        Profile Settings
+                    </NavLink>
                 </div>
             </div>
         </div>
