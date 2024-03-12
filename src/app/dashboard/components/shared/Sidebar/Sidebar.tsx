78dc58eeab703e8f05dcdad9fc85dbe9e8167186
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-12 14:42:21 +0600

    add: logo on the sidebar and design layout for sidebar

--- src/app/dashboard/components/shared/Sidebar/Sidebar.tsx
+++ src/app/dashboard/components/shared/Sidebar/Sidebar.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import logo from '@/assets/images/logo/BarberVoyageLogo.png'
+import Link from 'next/link';
+import Image from 'next/image';
 const Sidebar = () => {
     return (
-        <div>
-            Sidebar
+        <div className='fixed flex flex-col h-full w-full lg:w-80'>
+            <div className='border border-green-300 flex items-center justify-center my-2'>
+                <Link href={'/'} className='w-32'>
+                    <Image width={126} src={logo} alt='Logo of barber voyage' />
+                </Link>
+            </div>
+            <div className='bg-slate-200 flex-1'>
+                main
+            </div>
+            <div>
+                bottom
+            </div>
         </div>
     );
 };
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,10 +4,10 @@ import Sidebar from './components/shared/Sidebar/Sidebar';
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='flex'>
-            <div className='lg:w-80 bg-slate-300 fixed min-h-screen'>
+            <div className='lg:w-80 bg-seconderyCol min-h-screen'>
                 <Sidebar />
             </div>
-            <div className='flex-1 bg-slate-500 min-h-screen'>
+            <div className='flex-1 min-h-screen'>
                 {children}
             </div>
         </div>
