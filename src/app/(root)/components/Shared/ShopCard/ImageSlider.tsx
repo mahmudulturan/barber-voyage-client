ad6d6d1f5629c6330f0ab1af26ed0f80c9a76135
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-12 13:01:22 +0600

    change pagination colour on image carousel

--- src/app/(root)/components/Shared/ShopCard/ImageSlider.tsx
+++ src/app/(root)/components/Shared/ShopCard/ImageSlider.tsx
@@ -11,19 +11,24 @@ import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 const ImageSlider = ({ images, shopName, _id }: { images: string[], shopName: string, _id: string }) => {
     const swiperRef = useRef<SwiperRef | null>(null);
 
+    // for change slider pagination color
     useEffect(() => {
-        const bullets = document.querySelectorAll('.swiper-pagination-bullet');
-        bullets.forEach(bullet => {
-            (bullet as HTMLElement).style.backgroundColor = '#F1775C';
-        });
+        if (swiperRef.current) {
+            const bullets = swiperRef.current.swiper.pagination.bullets;
+            bullets.forEach(bullet => {
+                (bullet as HTMLElement).style.backgroundColor = '#F8F8F8';
+            });
+        }
     }, []);
 
+    // handler for previous slide changer
     const handlePrevSlide = () => {
         if (swiperRef.current) {
             swiperRef.current.swiper.slidePrev();
         }
     }
 
+    // handler for next slide changer
     const handleNextSlide = () => {
         if (swiperRef.current) {
             swiperRef.current.swiper.slideNext();
@@ -39,7 +44,10 @@ const ImageSlider = ({ images, shopName, _id }: { images: string[], shopName: st
                 modules={[Pagination]}
                 className="mySwiper rounded-md"
                 ref={swiperRef}
+                autoplay={false}
+                allowTouchMove={false}
             >
+                {/* map all images */}
                 {
                     images?.map((image, indx) => <SwiperSlide key={indx}>
                         <Link href={`/explore/shop/${_id}`}>
@@ -50,12 +58,16 @@ const ImageSlider = ({ images, shopName, _id }: { images: string[], shopName: st
                     </SwiperSlide>)
                 }
             </Swiper>
+
+            {/* navigation button for change previous slider */}
             <Button className='absolute top-[110px] mx-2 z-30 left-0' onClick={handlePrevSlide} variant={"icon"}>
                 <GrFormPrevious></GrFormPrevious>
             </Button>
+            {/* navigation button for change next slider */}
             <Button className='absolute top-[110px] mx-2 z-30 right-0' onClick={handleNextSlide} variant={"icon"}>
                 <GrFormNext></GrFormNext>
             </Button>
+
         </div>
     );
 };
