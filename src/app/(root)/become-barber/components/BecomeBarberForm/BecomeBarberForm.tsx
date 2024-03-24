0456324057cca522ed94c572aba1df9bbd8b10e0
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-03-25 01:34:48 +0600

    add: user name and email field in become barber form

--- src/app/(root)/become-barber/components/BecomeBarberForm/BecomeBarberForm.tsx
+++ src/app/(root)/become-barber/components/BecomeBarberForm/BecomeBarberForm.tsx
@@ -1,13 +1,18 @@
 "use client"
 import Button from '@/components/shared/Button/Button';
+import { Input } from '@/components/shared/Input/Input';
 import { barberSpecialties } from '@/constant/constant';
+import { RootState } from '@/redux/store';
 import Image from 'next/image';
 import React from 'react';
 import { FaUpload } from "react-icons/fa6";
+import { useSelector } from 'react-redux';
 
 const BecomeBarberForm = () => {
+    const { user: userInfo } = useSelector((state: RootState) => state.usersSlice);
+    console.log(userInfo);
     return (
-        <form className='max-w-2xl mx-auto my-12'>
+        <form className='max-w-2xl mx-auto my-16 '>
 
             {/* personal image and document update section start */}
             <div className='flex flex-col md:flex-row gap-6 justify-between items-center my-6'>
@@ -26,8 +31,15 @@ const BecomeBarberForm = () => {
             </div>
             {/* personal image and document update section end */}
 
+            {/* name and email info start */}
+            <div className='flex flex-col  gap-6 mt-6'>
+                <Input type='text' defaultValue={userInfo?.email} readOnly />
+                <Input type='text' defaultValue={userInfo?.name} readOnly />
+            </div>
+            {/* name and email info end */}
+
             {/* experience and speacilites select section start */}
-            <div className='flex flex-col md:flex-row gap-6 mt-6'>
+            <div className='flex flex-col  gap-6 mt-6'>
                 <select name="" id="" defaultValue={""} className='flex w-full rounded-md border border-input bg-white py-2 px-2 outline-none'>
                     <option value="" disabled>Your Experiences</option>
                     <option value="1/2 Years+">1/2 Years+</option>
@@ -46,7 +58,6 @@ const BecomeBarberForm = () => {
                 </select>
             </div>
             {/* experience and speacilites select section end */}
-
             <Button variant={"primaryReverse"} className='w-full my-6' type='submit'>Register as Barber</Button>
         </form>
     );
