762e1191eaeb7ebd273406ca947e883cbd34b602
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-20 01:20:59 +0600

    fix: dashboard-page-wrapper utility class

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,10 +4,10 @@ import Sidebar from './components/shared/Sidebar/Sidebar';
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='flex flex-col lg:flex-row'>
-            <div className='lg:min-h-screen relative'>
+            <div className='lg:min-h-screen lg:w-80 relative'>
                 <Sidebar />
             </div>
-            <div className='flex-1 min-h-screen'>
+            <div className='flex-1 min-h-screen border'>
                 {children}
                 <div className="min-h-screen"></div>
                 <div className="min-h-screen"></div>
